5769398f6bdb67a5ce7fca581aa4f5c98f476ce5
Kitsunegatari <andresfelipepinzoncamacho@gmail.com>
2026-03-03 20:18:36 -0500

    MVP

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,9 +1,9 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST as string,
+    user: process.env.DB_USER as string,
+    password: process.env.DB_PASSWORD as string,
+    database: process.env.DB_NAME as string,
 });
 console.log("DB_USER:", process.env.DB_USER);
